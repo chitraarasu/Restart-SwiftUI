e7b2e0fe31f90096b90c6533d21f8e45c51b9682
chitraarasu <kchitraarasu>
2023-02-01 18:45:47 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by kirshi on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
